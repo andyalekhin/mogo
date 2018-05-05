175ee24361d05673c03f83d03492d99646ebcfaf
Andy Alekhin <andy.alekhin@gmail.com>
2018-05-05 09:59:25 +0300

    add map and finished for large monitors

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,26 @@ var $grid = $('.grid').masonry({
 $grid.imagesLoaded().progress( function() {
     $grid.masonry('layout');
 });
+
+function initialize() {
+    if (!!document.getElementById('map')) {
+        initMap();
+    } else {
+        return false;
+    }
+}
+
+function initMap() {
+    var uluru = {lat: 40.7143528, lng: -74.0059731};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: uluru
+    });
+
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+        title: 'Mogo'
+    });
+}
