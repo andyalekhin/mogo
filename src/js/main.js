8ca9da96d17a4adaf3796582b517addf8b3d27a3
Andy Alekhin <andy.alekhin@gmail.com>
2018-05-13 23:50:19 +0300

    add responsive header

--- src/js/main.js
+++ src/js/main.js
@@ -39,3 +39,38 @@ function initMap() {
         title: 'Mogo'
     });
 }
+
+
+$(window).scroll(function() {
+	if ($(this).scrollTop() > 0) {
+		$('.header__fixed').addClass('header--fixed');
+	} else {
+		$('.header__fixed').removeClass('header--fixed');
+	}
+});
+
+$('.menu-collapsed').click(function() {
+	$(this).toggleClass('menu-expanded');
+});
+
+// $(function() {
+
+//     var w = $(window).width(),
+//       h = $(window).height();
+//     //$('section').width(w);
+//     $('section').height(h);
+//     $('menu .container').height(h - 60);
+//     $('body').prepend('<div id="overlay">');
+  
+//     $('#menu').click(function() {$('html').addClass('active');});
+//     $('#close-menu').click(function() {$('html').removeClass('active');});
+//     $('#overlay').click(function() {$('html').removeClass('active');});
+//     $(window).resize(function() {
+//       var w = $(window).width(),
+//         h = $(window).height();
+//       //$('section').width(w);
+//       $('section').height(h);
+//       $('menu .container').height(h - 60);
+//     });
+  
+//   });
